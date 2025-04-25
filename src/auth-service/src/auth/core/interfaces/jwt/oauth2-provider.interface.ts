741e9426af76a3d7254c08eb04694583eb5b9eba
Uinicivs <viniciushqalves@gmail.com>
2025-04-25 10:15:00 -0300

    feat(auth): implement core domain entities and interfaces

--- src/auth-service/src/auth/core/interfaces/jwt/oauth2-provider.interface.ts
+++
@@ -1,5 +0,0 @@
-import { IUserRequest } from '@/src/user/core/interfaces/user/user-request.interface';
-
-export interface IOAuthProvider {
-  fetchUserData(provider: string, token: string): Promise<IUserRequest>;
-}
--- src/auth-service/src/auth/core/interfaces/repositories/recovery-password.repository.ts
+++
@@ -1,7 +0,0 @@
-import { RecoverPassword } from '../../entities/recover-password.entity';
-
-export interface IRecoveryPasswordRepository {
-  create(recoveryPassword: RecoverPassword): Promise<RecoverPassword>;
-  revoke(id: string): Promise<RecoverPassword>;
-  findOne(token: string, email: string): Promise<RecoverPassword>;
-}
