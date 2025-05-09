19a5acf0a92e9eea23bebf0b82e3c044d21356ad
Siqueira2 <siqueiraguilherme920@gmail.com>
2025-05-09 15:20:00 -0300

    feat(db): add favorite recipes and view tracking tables

---
+++ src/auth-service/prisma/migrations/20241120050101_add_favourite_recipe_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "FavoriteRecipe" (
+    "userId" TEXT NOT NULL,
+    "recipeId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FavoriteRecipe_pkey" PRIMARY KEY ("userId","recipeId")
+);
+
+-- AddForeignKey
+ALTER TABLE "FavoriteRecipe" ADD CONSTRAINT "FavoriteRecipe_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FavoriteRecipe" ADD CONSTRAINT "FavoriteRecipe_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
