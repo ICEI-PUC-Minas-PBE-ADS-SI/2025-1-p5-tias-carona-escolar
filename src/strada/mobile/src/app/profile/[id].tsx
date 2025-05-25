e7ce76c2aaa59382429c25495ec0f443e5cd5993
Tiago1227 <tiago1227tc@gmail.com>
2025-05-25 12:20:00 -0300

    feat(mobile): implement navigation structure and routing

--- src/strada/mobile/src/app/profile/[id].tsx
+++
@@ -1,8 +0,0 @@
-import { UserDetailsScreen } from "@/src/screens/profile";
-import React from "react";
-
-const UserDetailsTab = () => {
-  return <UserDetailsScreen />;
-};
-
-export default UserDetailsTab;
