c87f354095846611249c0812ec831a18d3e4b5a0
lumachadoss <luisamachadossss@gmail.com>
2025-05-27 16:00:00 -0300

    feat(mobile): implement authentication and onboarding screens

--- src/mobile/src/screens/introduction-animation/scenes/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as SplashView } from './FirstView';
-export { default as SecondView } from './SecondView';
-export { default as CareView } from './ThirdView';
-export { default as MoodDiaryView } from './FourthView';
-export { default as WelcomeView } from './WelcomeView';
-export { default as TopBackSkipView } from './TopBackSkipView';
-export { default as CenterNextButton } from './CenterNextButton';
