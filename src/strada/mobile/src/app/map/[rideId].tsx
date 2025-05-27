f1c478a12ea0c59efd67caa43444d17543f12e5b
lumachadoss <luisamachadossss@gmail.com>
2025-05-27 06:32:24 -0300

    telinha de busca

--- src/strada/mobile/src/app/map/[rideId].tsx
+++ src/strada/mobile/src/app/map/[rideId].tsx
@@ -2,7 +2,7 @@ import RideShareMap from "@/src/screens/map/RideShareMap";
 import React from "react";
 
 const MapTab = () => {
-  return <RideShareMap rideId="cmbn4mbku000424zdh8s3eegl" />; //<MapboxMap />;
+  return <RideShareMap />; //<MapboxMap />;
 };
 
 export default MapTab;
