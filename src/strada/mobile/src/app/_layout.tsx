fdea2b09e7897db128967ea409158d1d8b1a3ec0
otaviogrocha <otaviogrocha@yahoo.com.br>
2025-05-31 04:21:27 -0300

    refactor: ensure that driver name exists and remove stack screen greetings

--- src/strada/mobile/src/app/_layout.tsx
+++ src/strada/mobile/src/app/_layout.tsx
@@ -3,10 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="greetings/[animValueParam]"
-        options={{ headerShown: false }}
-      />
       <Stack.Screen name="social-auth/[key]" options={{ headerShown: false }} />
       <Stack.Screen name="profile/[id]" options={{ headerShown: false }} />
       <Stack.Screen name="ride/[rideId]" options={{ headerShown: false }} />
